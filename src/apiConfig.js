ae1840d976febd9438fbe80b216a14c7d5fff3c9
kurotsubasa <kurotsubasa10@gmail.com>
2021-05-07 13:52:20 -0400

    Changed apiUrl
    Changed apiUrl

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
-  development: 'http://localhost:4741'
+  production: 'http://www.omdbapi.com/apikey.aspx',
+  development: 'http://www.omdbapi.com/apikey.aspx'
 }
 
 if (window.location.hostname === 'localhost') {
