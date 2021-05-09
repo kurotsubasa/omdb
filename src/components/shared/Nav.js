1dd99cf7684e1b359291f38894148d885d58d9f7
kurotsubasa <kurotsubasa10@gmail.com>
2021-05-09 19:06:23 -0400

    Removed Home button because redundant
    Removed Home button due to redundancy

--- src/components/shared/Nav.js
+++ src/components/shared/Nav.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+// import { NavLink } from 'react-router-dom'
 import Button from 'react-bootstrap/Button'
 
 const Nav = () => (
   <nav className='buttoins'>
     <Button
       variant="light">
-      <NavLink to='/movies'>Home</NavLink>
     </Button>
   </nav>
 )
